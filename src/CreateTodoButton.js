c241701bdc73fba10989a091764981bab914f892
Jose Luis <jose.lmy2004@gmail.com>
2023-06-26 12:54:53 -0500

    Maquetacion de los primeros componentes

---
+++ src/CreateTodoButton.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreateTodoButton() {
+    return(
+        <>
+            <button>CREAR</button>
+        </>
+    )
+}
+
+export {CreateTodoButton}
---
+++ src/TodoItem.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function TodoItem() {
+    return(
+        <>
+            <ul>
+                <span>V</span>
+                <h1>This is an earring</h1>
+                <span>X</span>
+            </ul>
+        </>
+    )
+}
+
+export {TodoItem}
